2ebc727d8742fe441d2a783161b6c95fd32b027d
carstenj <carstenj@gmail.com>
2015-09-11 20:11:06 +0200

    R environment setup

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+
+#export DOTFILES_DIR EXTRA_DIR
+#DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#EXTRA_DIR="$HOME/.extra"
+
+# Update dotfiles itself first
+
+#[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/R/.Renviron" ~
+
+#if [ "$(uname)" == "Darwin" ]; then
+#    ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
+#    if [ -f "$DOTFILES_DIR/install/osx.sh" ]; then
+#        . "$DOTFILES_DIR/install/osx.sh"
+#    fi
+#fi
+
+# Run tests
+
+#bats test/*.bats
